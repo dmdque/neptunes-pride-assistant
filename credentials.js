0f3089fd2d2b936d82a0098de30b7fa7d177a616
Daniel Que <dmdque@gmail.com>
2015-05-01 16:16:14 -0700

    added credentials file

---
+++ credentials.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // twilio
+  accountSid: 'AC50c6da0592315a23b99034168bf89eea',
+  authToken: '07fe549accd2e2c626c71bb8f2dcee38',
+  twilioTo: '+16473853783',
+  twilioFrom: '+16476913590',
+  // np2
+  cookie: 'auth=eyJfdXNlciI6WyJTSjlOTDRGVkNKS0pDUVY4M0FXVlIiLDEsInZmUkkzSmlSUVpVZ1EwbDd2TVNBdHYiLDE0MjQxNTcyMjQsMTQyNDE1NzIyNF19|1424157957|3eee36bff0c1423d31e294deaad7a1390419bdd2',
+  game_number: '6374248916451328'
+};
